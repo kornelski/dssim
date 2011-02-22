3612c0db2dc25cbac2489e851cc46fd930ffa903
porneL <pornel@pornel.net>
2011-02-22 21:59:50 +0000

    Split dssim and main

---
+++ dssim.h
@@ -0,0 +1,11 @@
+/*
+ *  dssim.h
+ *  dssim
+ *
+ *  Created by porneL on 22.lut.11.
+ *
+ */
+
+#include "rwpng.h"
+
+double dssim_image(read_info *image1, read_info *image2, const char *ssimfilename);
