2f4946b615667ef057da46d7915757fba556e064
Kornel Lesiński <kornel@geekhood.net>
2015-12-12 18:52:11 +0000

    Updated Rust API

--- src/dssim.rs
+++ src/dssim.rs
@@ -1,9 +1,13 @@
 extern crate libc;
 
+pub use ffi::dssim_ssim_map;
 pub use ffi::dssim_colortype::*;
 
-use libc::c_int;
+use libc::{c_int, c_uint};
 mod ffi;
+mod val;
+
+pub use val::Dssim as Val;
 
 #[allow(missing_copy_implementations)]
 pub struct Dssim {
@@ -17,6 +21,8 @@ pub struct DssimImage<'mem_src> {
 }
 
 pub type ColorType = ffi::dssim_colortype;
+
+pub type SsimMap = ffi::dssim_ssim_map;
 
 pub fn new() -> Dssim {
     Dssim::new()
@@ -35,6 +41,22 @@ impl Dssim {
         unsafe {
             ffi::dssim_set_scales(self.handle, scales.len() as c_int, scales.as_ptr());
         }
+    }
+
+    pub fn set_save_ssim_maps(&mut self, num_scales: u8, num_channels: u8) {
+        unsafe {
+            ffi::dssim_set_save_ssim_maps(self.handle, num_scales as c_uint, num_channels as c_uint);
+        }
+    }
+
+    pub fn pop_ssim_map(&mut self, scale_index: u8, channel_index: u8) -> Option<SsimMap> {
+        let m = unsafe {
+            ffi::dssim_pop_ssim_map(self.handle, scale_index as c_uint, channel_index as c_uint)
+        };
+        if 0 == m.width {
+            return None;
+        }
+        return Some(m);
     }
 
     pub fn create_image<'img, T>(&mut self, bitmap: &'img [T], color_type: ColorType, width: usize, stride: usize, gamma: f64) -> Option<DssimImage<'img>> {
@@ -66,13 +88,13 @@ impl Dssim {
         }
     }
 
-    pub fn compare(&mut self, original: &DssimImage, modified: DssimImage) -> f64 {
+    pub fn compare(&mut self, original: &DssimImage, modified: DssimImage) -> Val {
         assert!(!self.handle.is_null());
         assert!(!original.handle.is_null());
         assert!(!modified.handle.is_null());
         unsafe {
             ffi::dssim_compare(self.handle, original.handle, modified.handle)
-        }
+        }.into()
     }
 }
 
@@ -92,4 +114,29 @@ impl<'a> Drop for DssimImage<'a> {
             ffi::dssim_dealloc_image(self.handle);
         }
     }
+}
+
+#[cfg(test)]
+extern crate lodepng;
+
+#[test]
+fn test() {
+    let mut d = new();
+    let file1 = lodepng::decode32_file("test1.png").unwrap();
+    let file2 = lodepng::decode32_file("test2.png").unwrap();
+
+    let img1 = d.create_image(file1.buffer.as_ref(), DSSIM_RGBA, file1.width, file1.width*4, 0.45455).unwrap();
+    let img2 = d.create_image(file2.buffer.as_ref(), DSSIM_RGBA, file2.width, file2.width*4, 0.45455).unwrap();
+
+    let res = d.compare(&img1, img2);
+    assert!((0.015899 - res).abs() < 0.0001, "res is {}", res);
+    assert!(res < 0.0160);
+    assert!(0.0158 < res);
+
+    let img1b = d.create_image(file1.buffer.as_ref(), DSSIM_RGBA, file1.width, file1.width*4, 0.45455).unwrap();
+    let res = d.compare(&img1, img1b);
+
+    assert!(0.000000000000001 > res);
+    assert!(res < 0.000000000000001);
+    assert_eq!(res, res);
 }
---
+++ src/val.rs
@@ -0,0 +1,146 @@
+use ::std::fmt;
+use ::std::cmp::Ordering;
+use ::std::ops::Sub;
+use ::std::ops::Add;
+use ::std::ops::Mul;
+use ::std::ops::Div;
+
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
+pub struct Dssim(f64);
+
+impl Dssim {
+    pub fn new(v: f64) -> Dssim {
+        debug_assert!(v.is_finite());
+        Dssim(v)
+    }
+}
+
+impl fmt::Display for Dssim {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(fmt, "{:.6}", self.0)
+    }
+}
+
+impl Eq for Dssim {
+    // Result never has NaN
+}
+
+impl PartialEq<Dssim> for f64 {
+    fn eq(&self, other: &Dssim) -> bool {
+        *self == other.0
+    }
+
+    fn ne(&self, other: &Dssim) -> bool {
+        *self != other.0
+    }
+}
+
+impl PartialEq<f64> for Dssim {
+    fn eq(&self, other: &f64) -> bool {
+        self.0 == *other
+    }
+
+    fn ne(&self, other: &f64) -> bool {
+        self.0 != *other
+    }
+}
+
+impl From<Dssim> for f64 {
+    fn from(s: Dssim) -> f64 {
+        s.0
+    }
+}
+
+impl From<f64> for Dssim {
+    fn from(s: f64) -> Dssim {
+        Dssim(s)
+    }
+}
+
+impl <RHS: Into<f64>> Sub<RHS> for Dssim {
+    type Output = f64;
+    fn sub(self, r: RHS) -> Self::Output {
+        let rval = r.into();
+        debug_assert!(rval.is_finite());
+        self.0.sub(rval)
+    }
+}
+
+
+impl Sub<Dssim> for f64 {
+    type Output = f64;
+    fn sub(self, r: Dssim) -> Self::Output {
+        self.sub(r.0)
+    }
+}
+
+impl <RHS: Into<f64>> Add<RHS> for Dssim {
+    type Output = f64;
+    fn add(self, r: RHS) -> Self::Output {
+        let rval = r.into();
+        debug_assert!(rval.is_finite());
+        self.0.add(rval)
+    }
+}
+
+impl Add<Dssim> for f64 {
+    type Output = f64;
+    fn add(self, r: Dssim) -> Self::Output {
+        self.add(r.0)
+    }
+}
+
+impl <RHS: Into<f64>> Mul<RHS> for Dssim {
+    type Output = Dssim;
+    fn mul(self, r: RHS) -> Self::Output {
+        let rval = r.into();
+        debug_assert!(rval.is_finite());
+        self.0.mul(rval).into()
+    }
+}
+
+impl Mul<Dssim> for f64 {
+    type Output = Dssim;
+    fn mul(self, r: Dssim) -> Self::Output {
+        self.mul(r.0).into()
+    }
+}
+
+impl <RHS: Into<f64>> Div<RHS> for Dssim {
+    type Output = f64;
+    fn div(self, r: RHS) -> Self::Output {
+        let rval = r.into();
+        debug_assert!(rval.is_finite() && rval != 0.);
+        self.0.div(rval)
+    }
+}
+
+impl Div<Dssim> for f64 {
+    type Output = f64;
+    fn div(self, r: Dssim) -> Self::Output {
+        debug_assert!(r.0 != 0.);
+        self.div(r.0)
+    }
+}
+
+impl PartialOrd<f64> for Dssim {
+    fn partial_cmp(&self, other: &f64) -> Option<Ordering> {
+        self.0.partial_cmp(other)
+    }
+
+    fn lt(&self, other: &f64) -> bool { self.0.lt(other) }
+    fn le(&self, other: &f64) -> bool { self.0.le(other) }
+    fn gt(&self, other: &f64) -> bool { self.0.gt(other) }
+    fn ge(&self, other: &f64) -> bool { self.0.ge(other) }
+}
+
+impl PartialOrd<Dssim> for f64 {
+    fn partial_cmp(&self, other: &Dssim) -> Option<Ordering> {
+        self.partial_cmp(&other.0)
+    }
+
+    fn lt(&self, other: &Dssim) -> bool { self.lt(&other.0) }
+    fn le(&self, other: &Dssim) -> bool { self.le(&other.0) }
+    fn gt(&self, other: &Dssim) -> bool { self.gt(&other.0) }
+    fn ge(&self, other: &Dssim) -> bool { self.ge(&other.0) }
+}
