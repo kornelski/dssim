f6f1d6f1b49a8245214a9acdf4d3a4125751aaca
Kornel Lesiński <kornel@geekhood.net>
2015-12-25 21:50:38 +0100

    License comment

--- src/dssim.rs
+++ src/dssim.rs
@@ -1,5 +1,23 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
+/*
+ * © 2011-2015 Kornel Lesiński. All rights reserved.
+ *
+ * This file is part of DSSIM.
+ *
+ * DSSIM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * DSSIM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the license along with DSSIM.
+ * If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ */
 
 extern crate libc;
 extern crate itertools;
