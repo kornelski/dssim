19ecc9b829eaed001f24180263974da55bfb050f
Kornel Lesiński <kornel@geekhood.net>
2016-07-03 19:21:13 +0100

    Dedup

--- src/linear.rs
+++ src/linear.rs
@@ -1,23 +1,28 @@
 use super::image::RGBAPLU;
 use rgb::*;
 
+fn make_lut() -> [f32; 256] {
+    let mut gamma_lut = [0f32; 256];
+
+    for i in 0..256 {
+        let s: f64 = i as f64 / 255.0;
+        gamma_lut[i] = if s <= 0.04045 {
+            s / 12.92
+        } else {
+            ((s + 0.055) / 1.055).powf(2.4)
+        } as f32
+    }
+
+    gamma_lut
+}
+
 pub trait ToRGBAPLU {
     fn to_rgbaplu(&self) -> Vec<RGBAPLU>;
 }
 
 impl ToRGBAPLU for [RGBA<u8>] {
     fn to_rgbaplu(&self) -> Vec<RGBAPLU> {
-        let mut gamma_lut = [0f32; 256];
-
-        for i in 0..256 {
-            let s: f64 = i as f64 / 255.0;
-            gamma_lut[i] = if s <= 0.04045 {
-                s / 12.92
-            } else {
-                ((s + 0.055) / 1.055).powf(2.4)
-            } as f32
-        }
-
+        let gamma_lut = make_lut();
         self.iter().map(|px|{
             let a_unit = px.a as f32 / 255.0;
             RGBAPLU {
@@ -32,16 +37,7 @@ impl ToRGBAPLU for [RGBA<u8>] {
 
 impl ToRGBAPLU for [RGB<u8>] {
     fn to_rgbaplu(&self) -> Vec<RGBAPLU> {
-        let mut gamma_lut = [0f32; 256];
-
-        for i in 0..256 {
-            let s: f64 = i as f64 / 255.0;
-            gamma_lut[i] = if s <= 0.04045 {
-                s / 12.92
-            } else {
-                ((s + 0.055) / 1.055).powf(2.4)
-            } as f32
-        }
+        let gamma_lut = make_lut();
 
         self.iter().map(|px|{
             RGBAPLU {
