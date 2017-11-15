9c71e44311af6e51cc4eaa650b6aa7fe76bf32c5
Kornel <kornel@geekhood.net>
2017-11-15 23:19:32 +0000

    Benchmark

---
+++ benches/compare.rs
@@ -0,0 +1,26 @@
+#![feature(test)]
+
+extern crate dssim;
+extern crate imgref;
+extern crate test;
+extern crate lodepng;
+use dssim::*;
+use imgref::*;
+use test::Bencher;
+
+fn load(path: &str) -> Result<ImgVec<RGBAPLU>, lodepng::Error> {
+    let image = lodepng::decode32_file(path)?;
+    Ok(Img::new(image.buffer.to_rgbaplu(), image.width, image.height))
+}
+
+#[bench]
+fn compare(bench: &mut Bencher) {
+    let mut attr = dssim::Dssim::new();
+    let other = &load("tests/test1-sm.png").unwrap();
+    let orig = attr.create_image(&load("tests/test2-sm.png").unwrap()).unwrap();
+
+    bench.iter(|| {
+        let modif = attr.create_image(other).unwrap();
+        attr.compare(&orig, modif)
+    });
+}
