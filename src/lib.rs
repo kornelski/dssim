5c53fb531ce1cb6780846341664543aa038fb2cc
Kornel <kornel@geekhood.net>
2017-12-09 00:40:57 +0000

    Bump

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! The library interface is awfully abstract, because it strives to efficiently, and very accurately,
 //! support several pixel types. It also allows replacing some parts of the algorithm with different implementations
 //! (if you need higher accuracy or higher speed).
+#![doc(html_logo_url = "https://kornel.ski/dssim/logo.png")]
 
 extern crate rgb;
 extern crate imgref;
