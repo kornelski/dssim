c60bc631a8dcc4f1266d03a1ec3447899f7ef90c
Kornel <kornel@geekhood.net>
2021-03-20 14:03:17 +0000

    Avoid adding alpha to RGB inputs

--- src/lib.rs
+++ src/lib.rs
@@ -6,23 +6,22 @@ use imgref::*;
 use load_image::*;
 use std::path::Path;
 
-fn load(path: &Path) -> Result<ImgVec<RGBAPLU>, lodepng::Error> {
+fn load(attr: &Dssim, path: &Path) -> Result<DssimImage<f32>, lodepng::Error> {
     let img = load_image::load_image(path, false)?;
-    match img.bitmap {
-        ImageData::RGB8(ref bitmap) => Ok(Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
-        ImageData::RGB16(ref bitmap) => Ok(Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
-        ImageData::RGBA8(ref bitmap) => Ok(Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
-        ImageData::RGBA16(ref bitmap) => Ok(Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
-        ImageData::GRAY8(ref bitmap) => Ok(Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
-        ImageData::GRAY16(ref bitmap) => Ok(Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
-        ImageData::GRAYA8(ref bitmap) => Ok(Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
-        ImageData::GRAYA16(ref bitmap) => Ok(Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
-    }
+    Ok(match img.bitmap {
+        ImageData::RGB8(ref bitmap) => attr.create_image(&Img::new(bitmap.to_rgblu(), img.width, img.height)),
+        ImageData::RGB16(ref bitmap) => attr.create_image(&Img::new(bitmap.to_rgblu(), img.width, img.height)),
+        ImageData::RGBA8(ref bitmap) => attr.create_image(&Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
+        ImageData::RGBA16(ref bitmap) => attr.create_image(&Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
+        ImageData::GRAY8(ref bitmap) => attr.create_image(&Img::new(bitmap.to_rgblu(), img.width, img.height)),
+        ImageData::GRAY16(ref bitmap) => attr.create_image(&Img::new(bitmap.to_rgblu(), img.width, img.height)),
+        ImageData::GRAYA8(ref bitmap) => attr.create_image(&Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
+        ImageData::GRAYA16(ref bitmap) => attr.create_image(&Img::new(bitmap.to_rgbaplu(), img.width, img.height)),
+    }.expect("infallible"))
 }
 
 /// Load PNG or JPEG image from the given path. Applies color profiles and converts to sRGB.
 #[inline]
 pub fn load_image(attr: &Dssim, path: impl AsRef<Path>) -> Result<DssimImage<f32>, lodepng::Error> {
-    let bitmap = load(path.as_ref())?;
-    Ok(attr.create_image(&bitmap).expect("current implementation is infallible"))
+    load(attr, path.as_ref())
 }
