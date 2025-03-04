f5895481abb256b90f1c43eb652f13ab868e142d
Kornel <kornel@geekhood.net>
2025-03-04 12:15:21 +0000

    Split bench

--- benches/compare.rs
+++ benches/compare.rs
@@ -15,9 +15,19 @@ fn compare(bench: &mut Bencher) {
     let attr = dssim::Dssim::new();
     let other = &load("tests/test1-sm.png").unwrap();
     let orig = attr.create_image(&load("tests/test2-sm.png").unwrap()).unwrap();
+    let modif = attr.create_image(other).unwrap();
 
     bench.iter(|| {
-        let modif = attr.create_image(other).unwrap();
-        attr.compare(&orig, modif)
+        attr.compare(&orig, modif.clone())
+    });
+}
+
+#[bench]
+fn create_image(bench: &mut Bencher) {
+    let attr = dssim::Dssim::new();
+    let img = &load("tests/test1-sm.png").unwrap();
+
+    bench.iter(|| {
+        attr.create_image(img)
     });
 }
